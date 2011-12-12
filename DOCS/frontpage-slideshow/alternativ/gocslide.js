eb035f78b5e206c5e72579c68519b341d5c9e6be
Felix Hofmann <felix.hofmann@rustyy.de>
2011-12-12 12:52:04 +0100

    - slideshow now working, some minor issues to be done

--- DOCS/frontpage-slideshow/alternativ/gocslide.js
+++ DOCS/frontpage-slideshow/alternativ/gocslide.js
@@ -1,72 +1,94 @@
+/**
+ * @ToDo: - looping by time
+ *        - create jquery-plugin
+ *        - ie-test
+ *        - round widths Math.round()
+ *        - make some things dynamic (eg maxwidth, heights, duration)
+ */
+
 $(document).ready(function() {
   
+  // Get all slides available.
   var slideAmount = $("#slideshow").children().length;
+  // Get containerwidth.
   var wrapperWidth = $("#slideshow").width();
-  var minWidth = $("#slideshow").width() / slideAmount;
+  // medWidth is the slider-elements, when all of them are collapsed.
+  var medWidth = $("#slideshow").width() / slideAmount;
+  // maxWidth is the maximum dimension a slider can grow.
   var maxWidth = 420;
+  // minWidth is the width all sibling sliders have, if one is shown max/active.
+  var minWidth = (wrapperWidth - maxWidth) / (slideAmount - 1);
+  // The index of the active slide.
   var currentIndex;
 
-  // all slides to be collapsed with specific css
+  // All slides to be collapsed with specific css.
   $("#slideshow .slide").each(function( i ){
     $(this).css({
-      width: minWidth + 'px', 
-      left: i*minWidth,
+      width: medWidth + 'px', 
+      left: i*medWidth,
       'z-index': i+1
     });
   });
+
   // mouseenter
   $("#slideshow .slide").mouseenter(function(){
     currentIndex = $(this).index();
+    // Push active Slide to maximum and reduce 
+    // the left-attribute to the width of elements before.
+    // eg. currentIndex = 2, minWidth=42 -> left=84px
     $(this).animate({
-      width: maxWidth + 'px', 
-      left: ((wrapperWidth - maxWidth)/slideAmount) * $(this).index() + 'px'
+      width: maxWidth + 'px',
+      left: minWidth * currentIndex + 'px'
     },{
       queue: false, 
       duration: 300
     });
-
+    
+    // Morph the siblings.
     $(this).siblings().each(function(i){
-      //console.log('siblingi: ' + i + '/// currentI: ' + currentIndex);
-      if (i <= currentIndex) {
-        left = i*((wrapperWidth - maxWidth)/slideAmount);
+      // Is sibling before acitve slide?
+      if (i < currentIndex) {
+        left = i * minWidth;
       }
-      else{
-      // ...
+      // If not, add maxWidth.
+      else {
+        left = maxWidth + (i * minWidth);
       }
-      
-      console.log(left);
-      
       $(this).animate({
-        width: (wrapperWidth - maxWidth)/slideAmount + 'px'
-        
+        width: minWidth + 'px',
+        left: left + 'px'
       }, {
         queue: false, 
         duration: 300
       });
     });
-
+  });  
+  
   // Mouseleave
-  }).mouseleave(function(){
-    
-    i = $(this).index();
+  $("#slideshow .slide").mouseleave(function(){
+
     $(this).animate({
-      width: minWidth + 'px', 
-      left: minWidth * i + 'px'
+      width: medWidth + 'px', 
+      left: medWidth * currentIndex + 'px'
     }, {
       queue: false, 
       duration: 300
     });
-    $(this).siblings().each(function(){
+    
+    $(this).siblings().each(function(i){
+      if (i < currentIndex) {
+        left= i * medWidth;
+      }
+      else {
+        left = (i+1) * medWidth;
+      }
       $(this).animate({
-        width: minWidth + 'px'
+        width: medWidth + 'px',
+        left: left + 'px'
       }, {
         queue: false, 
         duration: 300
       });
     });
   });
-  
-  
-  
-
 });
