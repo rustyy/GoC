3245b2a0b7124a917279e5740885d4ed85ee77a3
Felix Hofmann <felix.hofmann@rustyy.de>
2011-12-19 01:41:09 +0100

    - removed merge-conflict-comments

--- docroot/sites/all/themes/goc/template.php
+++ docroot/sites/all/themes/goc/template.php
@@ -1,12 +1,6 @@
 <?php
-<<<<<<< HEAD
 function goc_preprocess_html(&$variables) {
   // add Google Webfonts
   $fonts = 'http://fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic|Nobile:400,400italic,700italic,700';
   drupal_add_css($fonts);
-=======
-
-function goc_name_preprocess_page(&$vars) {
-  kpr($vars);
->>>>>>> d2e7df09e6c4ce2d076ebe7bf3ab9c66f34d35e4
 }
