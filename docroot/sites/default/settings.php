d280b20e436c2c8e8cca3a8ea678624247e704a5
Acquia Cloud <support@acquia.com>
2012-02-11 00:02:50 +0000

    Importing site archive

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -24,4 +24,11 @@ ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
 
-require('/var/www/site-php/goc/goc-settings.inc');
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this 
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/goc/goc-settings.inc');
+}
