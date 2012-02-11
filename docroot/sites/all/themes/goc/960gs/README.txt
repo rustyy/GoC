ae6ffa1501fde53d4423beede1d39d1060520e8d
Felix Hofmann <felix.hofmann@rustyy.de>
2012-02-12 00:48:21 +0100

    - added taxonomy_menu module - added goc-theme

---
+++ docroot/sites/all/themes/goc/960gs/README.txt
@@ -0,0 +1,2 @@
+This is the basic 960gs CSS except of the following modifications:
+To fit drupal standard underscores have been replaced by hyphens.
---
+++ docroot/sites/all/themes/goc/plugins/layouts/site-template/site-template.tpl.php
@@ -0,0 +1,20 @@
+<div id="site" class="container-24">
+  <div class="grid-24">
+    <?php print $content['header']; ?>
+  </div>
+  <div class="grid-24">
+    <?php print $content['navigation']; ?>
+  </div>
+  <div class="grid-24">
+    <?php print $content['messages']; ?>
+  </div>
+  <div class="grid-16">
+    <?php print $content['content']; ?>
+  </div>
+  <div class="grid-8">
+    <?php print $content['sidebar']; ?>
+  </div>
+  <div class="grid-24">
+    <?php print $content['footer']; ?>
+  </div>
+</div>
