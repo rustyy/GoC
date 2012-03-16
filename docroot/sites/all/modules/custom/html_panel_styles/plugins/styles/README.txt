7437b1c211c537ffddb69c26e6a7867a0b27233c
Felix Hofmann <felix.hofmann@rustyy.de>
2012-03-16 15:46:42 +0100

    Theming
    - killed all field-markup produced by field.tpl.php
    - added "html-panel-styles" module

---
+++ docroot/sites/all/modules/custom/html_panel_styles/plugins/styles/README.txt
@@ -0,0 +1,1 @@
+olt -> one-line-teaser
---
+++ docroot/sites/all/modules/custom/html_panel_styles/plugins/styles/htags/htags-pane.tpl.php
@@ -0,0 +1,3 @@
+<?php print '<h' . $variables['settings']['htag'] . '>'; ?>
+  <?php print render($content->content); ?>
+<?php print '</h' . $variables['settings']['htag'] . '>'; ?>
