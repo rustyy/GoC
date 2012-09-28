87ab3538cd5fee3694fee6d59a1f87b9d2f2f6e2
Felix Hofmann <felix.hofmann@rustyy.de>
2012-09-28 23:33:10 +0200

    - set some ids in site-template - footer styling

--- sites/all/themes/goc/plugins/layouts/site-template/site-template.tpl.php
+++ sites/all/themes/goc/plugins/layouts/site-template/site-template.tpl.php
@@ -1,20 +1,25 @@
-<div id="header" >
+<div id="header">
   <?php print $content['header']; ?>
-  <div id="navigation" class="clearfix">
-    <?php print $content['navigation']; ?>
-  </div>
+    <div id="navigation" class="clearfix">
+      <?php print $content['navigation']; ?>
+    </div>
 </div>
 <div id="site" class="container-24">
-  <div class="grid-24">
-    <?php print $content['messages']; ?>
-  </div>
-  <div class="grid-16">
-    <?php print $content['content']; ?>
-  </div>
-  <div class="grid-8">
-    <?php print $content['sidebar']; ?>
-  </div>
-  <div class="grid-24">
-    <?php print $content['footer']; ?>
-  </div>
+    <div class="grid-24">
+      <?php print $content['messages']; ?>
+    </div>
+    <div class="clearfix">
+        <div id="content" class="grid-16">
+          <?php print $content['content']; ?>
+        </div>
+        <div id="sidebar" class="grid-8">
+          <?php print $content['sidebar']; ?>
+        </div>
+    </div>
+    <div id="footer" class="clearfix">
+        <div class="grid-24">
+          <?php print $content['footer']; ?>
+        </div>
+
+    </div>
 </div>
